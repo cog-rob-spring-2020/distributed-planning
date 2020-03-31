7c4c46597448ae6b9ccc8fee7b1134a258a0f793
Nicole <thumman@mit.edu>
2020-03-31 10:43:53 -0700

    Added  individual component and psuedocode for cooperative version

--- solutions/agent.py
+++ solutions/agent.py
@@ -1,8 +1,9 @@
 from plan import Path
 from pubsub import pub
+from cl_rrt import CL_RRT
 
 class Agent():
-    def __init__(id, other_ids):
+    def __init__(self, id, other_ids):
         """
         id - unique integer id
         other_ids - list of other agents' integer ids
@@ -24,6 +25,7 @@ class Agent():
 
         # attributes to support keeping a continually updated RRT process
         # TODO
+        self.current_position = None
 
         # constraints (will be updated according to other agents' paths)
         # TODO
@@ -38,6 +40,10 @@ class Agent():
     and calling Cam's CL-RRT code.
     """
     # TODO
+    steer(self):
+        # TODO:
+        # update self.current_position using self.plan
+        pass
 
     """
     Methods for listening and broadcasting to various topics.
@@ -45,16 +51,16 @@ class Agent():
     The listener methods comprise the interaction component of DMA-RRT
     and Cooperative DMA-RRT from Desaraju/How 2012, as described in algorithms 5 and 8 respectively.
     """
-    def broadcast_bid(bid):
+    def broadcast_bid(self, bid):
         pub.sendMessage("bids", self.id, bid)
 
-    def received_bid(bidder_id, bid):
+    def received_bid(self, bidder_id, bid):
         self.bids[bidder_id] = bid
 
-    def broadcast_waypoints(winner_id):
+    def broadcast_waypoints(self, winner_id):
         pub.sendMessage("waypoints", self.id, self.plan, winner_id)
 
-    def received_waypoints(other_id, other_plan, winner_id):
+    def received_waypoints(self, other_id, other_plan, winner_id):
         # TODO:
         # simulate other agent's trajectory according to their plan
         # and use it to update constraints for own planning
@@ -62,10 +68,10 @@ class Agent():
         if winner_id == self.id:
             self.token_holder = True
 
-    def broadcast_estop(stop_node):
+    def broadcast_estop(self, stop_node):
         pub.sendMessage("estop", stop_node)
 
-    def received_estop(stop_node):
+    def received_estop(self, stop_node):
         # terminate plan at specified node
         for i, node in enumerate(self.plan):
             if node == stop_node:
@@ -74,15 +80,26 @@ class Agent():
 
         self.check_estops = False
 
-    def individual():
+    def individual(self, bounds, environment, start_position, end_position, radius, delta_t):
         """
         Individual component of DMA-RRT as described in algorithm 4
         from Desaraju/How 2012.
         """
-        # TODO
-        pass
+        self.plan.add_node(start_position)
+        self.current_position = start_position
+        while True: #while active - need check for goal state
+            new_plan = CL_RRT(bounds, environment, self.current_position, end_position, radius=0.1, delta_t=0.1)
+            if self.token_holder:
+                self.plan = new_plan
+                winner = max(self.bids, key = lambda x: self.bids[x])
+                self.broadcast_waypoints(winner)
+                self.token_holder = False
+            else:
+                bid = self.plan.cost() - new_plan.cost()
+                self.broadcast_bid(bid)
 
-    def check_emergency_stops(best_new_plan, other_agent):
+
+    def check_emergency_stops(self, best_new_plan, other_agent):
         """
         Helper for the individual component of Cooperative DMA-RRT
         as described in algorithm 7 from Desaraju/How 2012.
@@ -101,10 +118,22 @@ class Agent():
 
         return best_new_plan, other_agent_modified
 
-    def coop_individual():
+    def coop_individual(self):
         """
         Individual component of Cooperative DMA-RRT as described
         in algorithm 6 from Desaraju/How 2012.
         """
-        # TODO
-        pass
+        while True: #while active - need check for goal state
+            new_plan = CL_RRT(bounds, environment, self.current_position, end_position, radius=0.1, delta_t=0.1)
+            if self.token_holder:
+                #check for first conflict (j)
+                    #check for second conflict (j')
+                #ID emergency stop nodes
+                self.plan = new_plan
+                #if modified angent js plan, j is winner, else:
+                winner = max(self.bids, key = lambda x: self.bids[x])
+                self.broadcast_waypoints(winner)
+                self.token_holder = False
+            else:
+                bid = self.plan.cost() - new_plan.cost()
+                self.broadcast_bid(bid)

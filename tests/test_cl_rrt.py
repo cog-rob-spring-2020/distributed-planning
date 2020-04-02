7fc9129b833cdc08f15870b5d17306550f9599bb
Cameron Pittman <cameron.w.pittman@gmail.com>
2020-04-01 21:14:37 -0400

    WIP - added notes about tests to write

---
+++ tests/test_cl_rrt.py
@@ -0,0 +1,8 @@
+from solutions.cl_rrt import tree_expansion
+
+
+def test_tree_expansion():
+    # start with a simple tree in a simple environment
+    # run check_path against all paths reaching the random point
+    # check cost estimates as euclidean distance
+    pass
--- tests/test_rrt.py
+++ tests/test_rrt.py
@@ -1,14 +1,14 @@
 from solutions.rrt import distance
 
-def test_distance():
+
+def test_generated_paths():
+    # create a loop with like 10 trials
+    # use a known feasible environment
+    # run RRT
+    # run check_path to see if it worked
+
     epsilon = 0.001
-    test_cases = [
-        {
-            "p1": (3, 3),
-            "p2": (4, 4),
-            "expected": 1.414,
-        }
-    ]
+    test_cases = [{"p1": (3, 3), "p2": (4, 4), "expected": 1.414,}]
 
     for tc in test_cases:
         found = distance(tc["p1"], tc["p2"])

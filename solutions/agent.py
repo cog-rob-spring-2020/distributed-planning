c5ba1dde95f9c81311074a60154b67f41dd490aa
Tanya Smith <tanika@mit.edu>
2020-04-04 23:51:01 -0400

    refactoring agent class to interface with RRTstar, include a spin method, and take other agent constraints into account

--- solutions/agent.py
+++ solutions/agent.py
@@ -1,9 +1,9 @@
-from plan import Path
+import time
 from pubsub import pub
-from cl_rrt import CL_RRT
+from rrtstar import Node, NodeStamped, RRTstar
 
 class Agent():
-    def __init__(self, id, other_ids):
+    def __init__(self, id, other_ids, x_start, y_start, x_goal, y_goal, environment, bounds, goal_dist, tolerance):
         """
         id - unique integer id
         other_ids - list of other agents' integer ids
@@ -16,34 +16,29 @@ class Agent():
         # (potential path improvement) at any given time:
         self.bids = {id:None for id in other_ids}
 
-        # initial state (e.g. pose)
-        # TODO
-        self.plan = Path()
+        # keeps track of other agents' plans so that we can
+        # add the other agents as obstacles when replanning
+        self.plans = {id:None for id in other_ids}
 
-        # model of vehicle dynamics
-        # TODO
+        # initial state and environment
+        self.tolerance = tolerance
+        self.goal_dist = goal_dist
+        self.bounds = bounds
+        self.environment = environment
 
-        # attributes to support keeping a continually updated RRT process
-        # TODO
-        self.current_position = None
+        self.pose = (x_start, y_start)
+        self.goal = (x_goal, y_goal)
 
-        # constraints (will be updated according to other agents' paths)
-        # TODO
+        self.rrt = RRTstar(self.pose, self.goal, self.environment, self.goal_dist)
+
+        # a list of NodeStamped waypoints, starting with current pose
+        self.plan = self.rrt.get_path()
 
         # register as listener for different kinds of messages
         pub.subscribe(self.received_bid, "bids")
         pub.subscribe(self.received_waypoints, "waypoints")
         pub.subscribe(self.received_estop, "estop")
 
-    """
-    Methods for simulating motion using vehicle dynamics
-    and calling Cam's CL-RRT code.
-    """
-    # TODO
-    steer(self):
-        # TODO:
-        # update self.current_position using self.plan
-        pass
 
     """
     Methods for listening and broadcasting to various topics.
@@ -61,10 +56,7 @@ class Agent():
         pub.sendMessage("waypoints", self.id, self.plan, winner_id)
 
     def received_waypoints(self, other_id, other_plan, winner_id):
-        # TODO:
-        # simulate other agent's trajectory according to their plan
-        # and use it to update constraints for own planning
-
+        self.plans[other_id] = other_plan
         if winner_id == self.id:
             self.token_holder = True
 
@@ -80,24 +72,39 @@ class Agent():
 
         self.check_estops = False
 
-    def individual(self, bounds, environment, start_position, end_position, radius, delta_t):
+    def refresh_constraints():
+        self.environment = Environment(bounds = self.bounds)
+        obstacles = []
+        for other_id, other_plan in self.plans.items():
+            # refresh agent's plan if they have already moved to their next node
+            if time.time() > other_plan[0].timestamp:
+                self.plans[other_id] = other_plan[1:]
+
+            # TODO add obstacle to obstacles representing agent at first node in its updated plan
+            # obstacles.append(agent at self.plans[other_id][0])
+
+        self.environment.add_obstacles(obstacles)
+
+    def individual(self):
         """
         Individual component of DMA-RRT as described in algorithm 4
         from Desaraju/How 2012.
         """
-        self.plan.add_node(start_position)
-        self.current_position = start_position
-        while True: #while active - need check for goal state
-            new_plan = CL_RRT(bounds, environment, self.current_position, end_position, radius=0.1, delta_t=0.1)
-            if self.token_holder:
-                self.plan = new_plan
-                winner = max(self.bids, key = lambda x: self.bids[x])
-                self.broadcast_waypoints(winner)
-                self.token_holder = False
-            else:
-                bid = self.plan.cost() - new_plan.cost()
-                self.broadcast_bid(bid)
+        # new_plan = CL_RRT(bounds, self.environment, self.pose, self.goal, radius=0.1, delta_t=0.1)
 
+        # refresh environment to reflect other agents' current positions
+        self.refresh_constraints()
+        self.rrt = RRTstar(self.pose, self.goal, self.environment, self.goal_dist)
+        new_plan = self.rrt.get_path()
+
+        if self.token_holder:
+            self.plan = new_plan
+            winner = max(self.bids, key = lambda x: self.bids[x])
+            self.broadcast_waypoints(winner)
+            self.token_holder = False
+        else:
+            bid = self.plan.cost() - new_plan.cost()
+            self.broadcast_bid(bid)
 
     def check_emergency_stops(self, best_new_plan, other_agent):
         """
@@ -123,17 +130,47 @@ class Agent():
         Individual component of Cooperative DMA-RRT as described
         in algorithm 6 from Desaraju/How 2012.
         """
-        while True: #while active - need check for goal state
-            new_plan = CL_RRT(bounds, environment, self.current_position, end_position, radius=0.1, delta_t=0.1)
-            if self.token_holder:
-                #check for first conflict (j)
-                    #check for second conflict (j')
-                #ID emergency stop nodes
-                self.plan = new_plan
-                #if modified angent js plan, j is winner, else:
-                winner = max(self.bids, key = lambda x: self.bids[x])
-                self.broadcast_waypoints(winner)
-                self.token_holder = False
+        # new_plan = CL_RRT(bounds, environment, self.current_position, end_position, radius=0.1, delta_t=0.1)
+
+        # refresh environment to reflect other agents' current positions
+        self.refresh_constraints()
+        self.rrt = RRTstar(self.pose, self.goal, self.environment, self.goal_dist)
+        new_plan = self.rrt.get_path()
+
+        if self.token_holder:
+            #check for first conflict (j)
+                #check for second conflict (j')
+            #ID emergency stop nodes
+            self.plan = new_plan
+            #if modified angent js plan, j is winner, else:
+            winner = max(self.bids, key = lambda x: self.bids[x])
+            self.broadcast_waypoints(winner)
+            self.token_holder = False
+        else:
+            bid = self.plan.cost() - new_plan.cost()
+            self.broadcast_bid(bid)
+
+    def spin(self, rate, coop = False):
+        """
+        Runs the agent's individual component on a timer until it is sufficiently close to the goal.
+
+        rate - rate at which the individual algorithm is run, in Hz
+        coop - indicates whether to run the Cooperative DMA-RRt extension
+        """
+        x, y = self.pose
+        x_goal, y_goal = self.goal
+
+        # go until agent has reached its goal state
+        while ((x - x_goal)**2 + (y - y_goal)**2) ** 1/2 > self.tolerance:
+            # move agent if we have reached the next node
+            if time.time() > self.plan[0].timestamp:
+                self.pose = plan[0]
+                self.plan = plan[1:]
+
+            # run one iteration of the individual method for DMA-RRT
+            if coop:
+                self.coop_individual()
             else:
-                bid = self.plan.cost() - new_plan.cost()
-                self.broadcast_bid(bid)
+                self.individual()
+
+            time.sleep(1 / rate)

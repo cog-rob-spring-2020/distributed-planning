e71dd48d372fa31e7f92e9160e481fd0e3283a8b
Nicole <thumman@mit.edu>
2020-05-02 15:06:30 -0700

    Restructed agent

--- scripts/agent_dmarrt.py
+++ scripts/agent_dmarrt.py
@@ -3,17 +3,50 @@ import random
 import rospy
 import yaml
 from distributed_planning.msg import *
-from agent import Agent
 from environment import Environment
+import argparse
+import time
+import numpy as np
+
+from plan import Plan
+from rrtstar import Node, NodeStamped, RRTstar, Path
 
 
-class DMARRTAgent(Agent):
+class DMARRTAgent(object):
     """
-    An Agent that uses DMA-RRT for distributed path planning
+    Represents a robot capable of
+    planning and moving in an environment using DMA-RRT.
+    Use as a base for decentralized planning algorithms.
     """
 
-    def __init__(self, *args, **kwargs):
-        super(DMARRTAgent, self).__init__(*args, **kwargs)
+    def __init__(self, start_pos, goal_pos, environment, goal_dist, rrt_iters):
+        """
+        start_pos - a 2-tuple representing the starting x-y position.
+        goal_pos - a 2-tuple representing the goal x-y position.
+        environment - an Environment object representing the map in
+            which the agent must plan.
+        goal_dist - a float representing the length of a single branch
+            in the RRT tree for the agent's planner.
+        rrt_iters - the number of iterations to run for RRT at each
+            spin_once.
+        """
+
+        # Initial state and environment
+        self.start = start_pos
+        self.pos = self.start
+
+        self.goal = goal_pos
+        self.goal_dist = goal_dist
+
+        self.environment = environment
+
+        self.rrt = RRTstar(self.start, self.goal, self.environment, goal_dist, max_iter=rrt_iters)
+
+        # curr_plan is the currently executing plan; best_plan is a
+        #    lower-cost path than curr_plan, if one exists.
+        #    The cost difference is the bid!
+        self.curr_plan = Path()
+        self.best_plan = Path()
 
         # a unique name given to us by ROS
         self.identifier = rospy.get_name()
@@ -23,9 +56,7 @@ class DMARRTAgent(Agent):
         self.plan_bid_pub = rospy.Publisher("plan_bids", PlanBid, queue_size=10)
 
         # be ready for updated waypoints from the winner
-        rospy.Subscriber(
-            "waypoints", Waypoints, self.received_waypoints_and_replan_winner
-        )
+        rospy.Subscriber("waypoints", Waypoints, self.received_waypoints_and_replan_winner)
         self.waypoints_pub = rospy.Publisher("waypoints", Waypoints, queue_size=10)
 
         # let the other agents know a new agent is on the network
@@ -44,9 +75,26 @@ class DMARRTAgent(Agent):
         # TODO: how should this be updated?
         self.curr_time = 0.0  # Simulation time. Updated externally
 
+    ####################################################################
+    
+    def broadcast_replan_bid(self, bid):
+        """
+        Broadcasts the following message to the plan_bids topic
+
+        msg - message of type PlanBid
+        msg.sender_id - this agent's unique ID
+        msg.bid - this agent's PPI, given by `bid`
+        """
+        msg = PlanBid()
+        msg.sender_id = self.identifier
+        msg.bid = bid
+        self.plan_bid_pub.publish(msg)
+
+    ####################################################################
+
     def received_registration(self, msg):
         """
-        We met a peer, let's note that we're tracking their path
+        We met a peer, let's note that we're tracking their path and bids
         """
         if msg.sender_id != self.identifier:
             self.peer_waypoints[msg.sender_id] = Path()
@@ -60,7 +108,8 @@ class DMARRTAgent(Agent):
         msg.bid - agent's PPI (potential path improvement) for current
         planning iteration
         """
-        self.plan_bids[msg.sender_id] = msg.bid
+        if msg.sender_id != self.identifier:
+            self.plan_bids[msg.sender_id] = msg.bid
 
     def received_waypoints_and_replan_winner(self, msg):
         """
@@ -78,21 +127,43 @@ class DMARRTAgent(Agent):
         """
         if msg.winner_id == self.identifier:
             self.plan_token_holder = True
+        if msg.sender_id != self.identifier:
+            self.peer_waypoints[msg.sender_id] = msg.waypoints
 
-        self.peer_waypoints[msg.sender_id] = msg.waypoints
+    ####################################################################
 
-    def broadcast_replan_bid(self, bid):
+    def at_goal(self):
         """
-        Broadcasts the following message to the plan_bids topic
-
-        msg - message of type PlanBid
-        msg.sender_id - this agent's unique ID
-        msg.bid - this agent's PPI, given by `bid`
+        Checks if the agent's current location is the goal location.
         """
-        msg = PlanBid()
-        msg.sender_id = self.identifier
-        msg.bid = bid
-        self.plan_bid_pub.publish(msg)
+        dist = np.sqrt(
+            (self.pos[0] - self.goal[0]) ** 2 + (self.pos[1] - self.goal[1]) ** 2
+        )
+        if dist <= 0.3:
+            return True
+        return False
+    
+    def update_time(self, curr_time):
+        """
+        Update the internal time counter.
+        """
+        self.curr_time = curr_time
+
+    def create_new_plan(self):
+        """
+        Spin RRT to create a new plan
+
+        Returns:
+            Path
+        """
+        # Refresh environment to reflect agent's current positions
+        self.rrt.update_pos(self.pos, self.curr_time, wipe_tree=True)
+
+        # Grow the tree by one set of iterations
+        self.rrt.spin(False)
+
+        # Find the new best path in the tree
+        return self.rrt.get_path()
 
     def spin_once(self):
         """
@@ -121,30 +192,32 @@ class DMARRTAgent(Agent):
 
             # Solve collisions with time reallocation
             self.curr_plan = Plan.multiagent_aware_time_realloc(
-                self.curr_plan, self.other_agent_plans
+                self.curr_plan, self.peer_waypoints
             )
 
             # Broadcast the new winner of the bidding round
+            winner_id = self.identifier #default to being the winner again
             if len(self.plan_bids) > 0:
                 # select a winner based on bids
-                winner_bid = max(self.bids.values())
-                winner_ids = [id for id, bid in self.bids.items() if bid == winner_bid]
+                winner_bid = max(self.plan_bids.values())
+                winner_ids = [id for id, bid in self.plan_bids.items() if bid == winner_bid]
                 # break bid ties with randomness
                 winner_id = random.choice(winner_ids)
-
-                # broadcast own waypoints and new token holder
-                msg = Waypoints()
-                msg.sender_id = winner_id
-                msg.waypoints = self.best_plan
-                self.waypoints_pub.publish(msg)
-
                 self.plan_token_holder = False
+
+            # broadcast own waypoints and new token holder
+            msg = Waypoints()
+            msg.sender_id = self.identifier
+            msg.winner_id = winner_id
+            msg.waypoints = self.best_plan
+            self.waypoints_pub.publish(msg)
+
         else:
             self.broadcast_replan_bid(self.curr_plan.cost - self.best_plan.cost)
 
         if self.at_goal():
             # no more replanning necessary!
-            self.broadcast_bid(-1000.0)
+            self.broadcast_replan_bid(-1000.0)
 
 
 if __name__ == "__main__":
